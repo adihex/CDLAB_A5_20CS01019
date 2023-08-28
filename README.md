82031fcd3afa012fedbef433fc661d66cf420405
adihex <aditya.balakrishnan530@gmail.com>
2023-08-28 18:51:18 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Compiler Design Laboratory 
+## Assignment 5
+
+`Name: Aditya Balakrishnan
+ Roll No: 20CS01019`
---
+++ q5.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+#define EXIT_SUCCESS 0
+int main(int argc, char *argv[]) { return EXIT_SUCCESS; }
